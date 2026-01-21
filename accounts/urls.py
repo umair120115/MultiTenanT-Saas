b1eac56d5a1a2fdedf953a38c4571826ff10d5e1
Umair Ahmad <ua16453@gmail.com>
2026-01-21 18:19:56 +0530

    Corrected Import

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.view import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='access-token'),
     path('refresh/token/', TokenRefreshView.as_view(), name='refresh-token'),
