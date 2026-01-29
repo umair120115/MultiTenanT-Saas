9eb04d7a5568ab3e33e3b1833086fef2cd4a9a0c
Umair Ahmad <ua16453@gmail.com>
2026-01-29 19:34:12 +0530

    Compnay Listing API

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from django.core.cache import cache
 # Create your views here.
+from .models import Company
+from rest_framework.pagination import PageNumberPagination
+from .serializers import CompanySerializer
+class CompanyAdminPagination(PageNumberPagination):
+    page_size=20
+    page_size_query_param='page_size'
+    max_page_size=100
+
+class CompanyListView(generics.ListAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all()
+    pagination_class = CompanyAdminPagination
+
