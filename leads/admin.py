3a1aad15c29ccdf205186bf4b77c6cb92b9e0582
Umair Ahmad <ua16453@gmail.com>
2026-02-09 17:38:27 +0530

    Changed Admin display columns in TempCSVFile Uploads

--- leads/admin.py
+++ leads/admin.py
@@ -3,7 +3,10 @@ from .models import Lead , TempCSVImport, UpdateLeadNotes
 # Register your models here.
 # admin.site.register(Lead)
 
-admin.site.register(TempCSVImport)
+# admin.site.register(TempCSVImport)
+@admin.register(TempCSVImport)
+class TempCSVAdmin(admin.ModelAdmin):
+    list_display =('id', 'file','uploaded_at')
 @admin.register(Lead)
 class LeadAdmin(admin.ModelAdmin):
     list_display = ('id','status')
