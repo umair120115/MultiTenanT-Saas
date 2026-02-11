880b7de020baf0364e1dbe3327bf9e223041ae50
Umair Ahmad <ua16453@gmail.com>
2026-02-11 22:25:55 +0530

    .

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import AuthenticateView , UserOnBoardingView
+from .views import AuthenticateView , UserOnBoardingView, health_check
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='access-token'),
     path('refresh/token/', TokenRefreshView.as_view(), name='refresh-token'),
     path('login/v2/', AuthenticateView.as_view(), name="Authenticate"),
     path('signup/', UserOnBoardingView.as_view(), name="user-onboarding"),
+    path('health/', health_check, name="health-check"),
     
 
 ]
